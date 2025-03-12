499e60c0ab7d070132025d4f8848c2c17f2ab95c
Sirojul Munir <rojulman@gmail.com>
2025-03-12 09:11:25 +0700

    belajar php oop

---
+++ nilai_mahasiswa.php
@@ -0,0 +1,19 @@
+<?php 
+class NilaiSiswa {
+    public $nama;
+    public $nilai;
+    public $nilai_uts;
+    public $nilai_uas;
+    public $nilai_tugas;
+    public const PERSENTASE_UTS = 0.25;
+    public const PERSENTASE_UAS = 0.30;
+    public const PERSENTASE_TUGAS = 0.45;
+
+    public function getNilaiAkhir(){
+        $nilai_akhir = $this->nilai_uts * self::PERSENTASE_UTS +
+          $this->nilai_uas * self::PERSENTASE_UAS +
+          $this->nilai_tugas * self::PERSENTASE_TUGAS;
+        return $nilai_akhir;
+    }
+
+}
